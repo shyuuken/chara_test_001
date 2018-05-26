98a050b3fbdbf999bcbacb9d9903732b4232e436
Taku Funami <fff273109@gmail.com>
2018-05-26 16:24:24 +0900

    chara_test_001 プロジェクト　初回コミット
    Unityちゃんにアニメーションを設定するために作成したプロジェクトです。
    マウスで画面をクリックすると待機アニメーション(Idle)から攻撃用のアニメーション(atk_001)に遷移するようにしようと思いましたが、
    現在クリックした際に一度だけしかアニメーションが再生されていません。

---
+++ Assets/EXT_Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EXT_Controller : MonoBehaviour {
+    bool isAttack;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void FinishAttack()
+    {
+        isAttack = false;
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+        //  マウスがクリックされた場合
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(!isAttack)
+            {
+                isAttack = true;
+                //Animatorコンポーネントを取得し、"atk_001_Trigger"をtrueにする
+                GetComponent<Animator>().SetTrigger("atk_001_Trigger");
+            }
+            
+        }
+
+
+
+    }
+}
