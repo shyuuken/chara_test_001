979a6fc4f7d1b444d48b9d2eaa3003e2b44bd0d5
Taku Funami <fff273109@gmail.com>
2018-06-07 08:32:33 +0900

    chara_test_001 敵となるオブジェクトのコントロール
    敵となるオブジェクトの動作を決めようとしていました。
    最初のうち中央に配置したキャラクターをターゲットにしようと思いましたが、
    中央で止まらずキャラクターをすり抜けて画面外に出るよう変更しようと思い
    DestroyWallを作成しターゲットにしようとしています。

--- Assets/cube_con_2.cs
+++ Assets/cube_con_2.cs
@@ -7,26 +7,64 @@ public class cube_con_2 : MonoBehaviour
 
     public float speed = 1.0f;
 
-    private GameObject player;
+    //private GameObject player;
+
+    private GameObject wall;
+
+    Vector3 target = new Vector3(0, 0, 0.1f);
+
+  
 
     // Use this for initialization
     void Start() {
-        player = GameObject.FindGameObjectWithTag("Player");
+        //player = GameObject.FindGameObjectWithTag("Player");
+        wall = GameObject.FindGameObjectWithTag("DestroyWall");
 
     }
+
 
     //クリックしたオブジェクトを消す
     void OnMouseDown()
     {
-        GameObject.Find("EXT_UNT").GetComponent<EXT_Controller>().LookAt(transform.position);
+        //GameObject.Find("EXT_UNT").GetComponent<EXT_Controller>().LookAt(transform.position);
         Destroy(this.gameObject,0.5f);
     }
 
-
     // Update is called once per frame
     void Update()
-    {
+        {
+        transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime);
 
-        
-    }
+
+        /*
+        Vector3 pos = transform.position;
+        pos.z -= 10;
+        transform.position = pos;
+        */
+        }
+
+
+    /*
+   {
+   //ターゲットのほうに少しずつ向きを変える
+   //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target)
+
+   //ターゲットに向かって進む
+   //transform.position = player.transform.position;
+
+   //ターゲットの位置からyに+1.3の位置
+   Vector3 pos = transform.position;
+   pos.y = 1.3f;
+   transform.position = pos;
+
+   //ターゲットの位置からyに+1.3の位置に向かって進む
+   //transform.position = GameObject.Find("Player").transform.position;
+   //GameObject.Find("Player").transform.position = new Vector3(tmp.x, tmp.y + 1.3f, tmp.z);
+   }
+   */
+
+
+
+
+
 }
